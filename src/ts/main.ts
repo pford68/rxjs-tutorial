4d6dcaae961e5d92c5b82a2f9d067971c1ed1294
Philip Ford <pford68@earthlink.net>
2017-01-09 01:28:54 -0500

    Improved AJAx handling with RxJS

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -10,25 +10,32 @@ let button = document.getElementById('button');
 let source = Observable.fromEvent(button, "click")
 
 function load(url: string){
-    let xhr = new XMLHttpRequest();
+    return Observable.create(observer => {
+        let xhr = new XMLHttpRequest();
 
-    xhr.addEventListener('load', () => {
-       let data = JSON.parse(xhr.responseText);
-       data.movies.forEach(e => {
-           let div = document.createElement('div');
-           div.textContent = e.title;
-           output.appendChild(div);
-       });
+        xhr.addEventListener('load', () => {
+            let data = JSON.parse(xhr.responseText);
+            observer.next(data);
+            observer.complete();
+        });
+
+        xhr.open('GET', url);
+        xhr.send();
+    })
+}
+
+function renderMovies(data){
+    data.movies.forEach(e => {
+        let div = document.createElement('div');
+        div.textContent = e.title;
+        output.appendChild(div);
     });
-
-
-    xhr.open('GET', url);
-    xhr.send();
 }
 
 
-source.subscribe(
-    e => load('data.json'),
+
+source.flatMap(e => load('data.json')).subscribe(
+    renderMovies,
     e => console.log(`error:  ${e}`),
     () => console.log('complete')
 );
